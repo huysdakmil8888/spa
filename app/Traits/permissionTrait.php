645ecbe99627eb2ccd2b5494c6b26b18551a6b61
huysdakmil8888 <80471364+huysdakmil8888@users.noreply.github.com>
2021-08-18 15:42:01 +0700

    Update permissionTrait.php

--- app/Traits/permissionTrait.php
+++ app/Traits/permissionTrait.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 trait permissionTrait{
    public function hasPermission()
    {
-      if(!isset(auth()->user()->role->permission['name']['permission']['can-edit']) && Route::is('permissions.edit') ){
+      if(!isset(auth()->user()->role->permission['name']['department']['can-edit']) && Route::is('permissions.edit') ){
          return abort(401);
       }
-      if(!isset(auth()->user()->role->permission['name']['permission']['can-list']) && Route::is('permissions.index') ){
+      if(!isset(auth()->user()->role->permission['name']['department']['can-list']) && Route::is('permissions.index') ){
          return abort(401);
       }
-      if(!isset(auth()->user()->role->permission['name']['permission']['can-add']) && Route::is('permissions.create') ){
+      if(!isset(auth()->user()->role->permission['name']['department']['can-add']) && Route::is('permissions.create') ){
          return abort(401);
       }
 
